ba418a252fc42c439892ad97bc166b777456fd0e
bonchovylkov <bonchovylkov@abv.bg>
2013-04-18 02:24:24 +0300

    Update Size.cs

--- usingDataConstantsVariables/usingDataConstantsVariables/Size.cs
+++ usingDataConstantsVariables/usingDataConstantsVariables/Size.cs
@@ -1,7 +1,7 @@
 ﻿public class Size
 {
-    public double Width {get;set;};
-    public double Height{get;set;};
+    public double Width {get;set;}
+    public double Height{get;set;}
     public Size(double width, double hight)
     {
         this.Width = width;
